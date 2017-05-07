c201c79910db49d13afd5659ceb9bfc38f5467cf
whispeer <ertyu>
2017-05-07 21:54:35 +0300

    first

---
+++ WebApplication2/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    [RoutePrefix("api")]
+
+    public class DefaultController : ApiController
+    {
+        [Route("get")] 
+        public string Get() {
+            return "one branch (new)";
+        }            
+    }
+}
